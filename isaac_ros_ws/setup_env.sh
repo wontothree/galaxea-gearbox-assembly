9a362d71d1d06decab37439618a82182244c8715
Junwoo Hwang <junwoo091400@gmail.com>
2026-01-13 00:12:15 +0900

    Update Isaac ROS docker and scripts + add RVIZ config

--- isaac_ros_ws/setup_env.sh
+++ isaac_ros_ws/setup_env.sh
@@ -16,3 +16,5 @@ cd ${ISAAC_ROS_WS} && \
    colcon build --symlink-install --packages-up-to isaac_ros_rtdetr --base-paths ${ISAAC_ROS_WS}/src/isaac_ros_object_detection/isaac_ros_rtdetr
 
 source ${ISAAC_ROS_WS}/install/setup.bash
+
+apt-get install -y ros-jazzy-isaac-ros-examples
